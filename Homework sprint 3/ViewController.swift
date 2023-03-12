b7736abbaabcd420844a8fafbab96fc52387f5a5
Gleb Nikiforov <nikiformif@gmail.com>
2023-03-12 20:53:55 +0700

    Первый commit

--- Homework sprint 3/ViewController.swift
+++ Homework sprint 3/ViewController.swift
@@ -9,11 +9,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyTextLable: UITextView!
+    
+    var x = 0
+    func publicTime() -> String {
+        var date = Date()
+        var calendar = Calendar.autoupdatingCurrent
+        var hours = calendar.component(.hour, from: date)
+        var minets = calendar.component(.minute, from: date)
+        var seconds = calendar.component(.second, from: date)
+        var time = "\(hours):\(minets):\(seconds)"
+        return time
+    }
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counterLabel.text = "Значение счётчика: \(x)"
+        plusButton.backgroundColor = .red
+        minusButton.backgroundColor = .blue
+        historyTextLable.
+        
+        
     }
+    
 
-
+    @IBAction func pressPlusButton(_ sender: Any) {
+        x += 1
+        print(x)
+        counterLabel.text = "Значение счётчика: \(x)"
+        historyTextLable.insertText("\n \(publicTime()): значение изменено на +1")
+        
+    }
+    
+    @IBAction func pressMinusButton(_ sender: Any) {
+        if x <= 0 {
+            x = 0
+            counterLabel.text = "Значение счётчика: \(x)"
+            historyTextLable.insertText("\n \(publicTime()): попытка уменьшить значение счётчика ниже 0")
+        } else {
+            x -= 1
+            counterLabel.text = "Значение счётчика: \(x)"
+            historyTextLable.insertText("\n \(publicTime()): значение изменено на -1")
+        }
+    }
+    
+    @IBAction func pressReset(_ sender: Any) {
+        x = 0
+        counterLabel.text = "Значение счётчика: \(x)"
+        historyTextLable.insertText("\n \(publicTime()): значение сброшено")
+    }
+    
+    
 }
 
