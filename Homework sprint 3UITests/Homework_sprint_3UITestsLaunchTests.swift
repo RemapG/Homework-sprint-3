c41335540c3c87a279616ed16e08c86bb155a92e
Gleb Nikiforov <nikiformif@gmail.com>
2023-03-12 18:43:06 +0700

    Initial Commit

---
+++ Homework sprint 3UITests/Homework_sprint_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Homework_sprint_3UITestsLaunchTests.swift
+//  Homework sprint 3UITests
+//
+//  Created by Wood Baze on 12.03.2023.
+//
+
+import XCTest
+
+final class Homework_sprint_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
